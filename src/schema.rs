0b7dca7cf55ab9076568a9048e475e2bee294c37
Drazen Urch <github@drazenur.ch>
2017-05-16 14:32:06 +0200

    No connection needed at compile time

--- src/schema.rs
+++ src/schema.rs
@@ -1,1 +1,13 @@
-infer_schema!("dotenv:DATABASE_URL");
+// infer_schema!("dotenv:DATABASE_URL");
+
+table! {
+    checks (id) {
+        id -> Int4,
+        url -> Text,
+        rate -> Int4,
+        last_start -> Nullable<Timestamptz>,
+        last_end -> Nullable<Timestamptz>,
+        http_status -> Nullable<Int4>,
+        state -> Nullable<Text>,
+    }
+}
